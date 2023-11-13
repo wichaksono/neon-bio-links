a0c5bcd5848ec4c3783b09477bc0de37dc08e706
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2023-11-13 14:40:20 +0700

    Add files via upload

---
+++ src/Classes/Admin.php
@@ -0,0 +1,9 @@
+<?php
+namespace NeonBioLinks\Src\Classes;
+
+class Admin {
+
+    public static function init() {
+        print_r( get_declared_classes() );
+    }
+}
---
+++ src/Classes/ClassOne.php
@@ -0,0 +1,9 @@
+<?php
+namespace NeonBioLinks\Src\Classes;
+
+class ClassOne extends Admin {
+
+    public function __construct() {
+        echo 'class one';
+    }
+}
